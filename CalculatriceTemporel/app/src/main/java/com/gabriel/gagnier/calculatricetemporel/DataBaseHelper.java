32724eba3f188cbc0925a4a4aa52b38e28bb6d5b
ThibaultJouan <thibaultjouan@hotmail.com>
2016-11-14 12:00:01 +0100

    Ajout DB helper

---
+++ CalculatriceTemporel/app/src/main/java/com/gabriel/gagnier/calculatricetemporel/DataBaseHelper.java
@@ -0,0 +1,56 @@
+package com.gabriel.gagnier.calculatricetemporel;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by thibault on 14/11/2016.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, DATABASE_VERSION);
+    }
+
+    private static final String DB_NAME = "Calculatrice_Temporelle.db";
+
+    private static final String TABLE_EVENEMENTS = "evenements";
+    private static final String COLUMN_ID = "_id";
+    private static final String LIBELLE = "libelle";
+    private static final String DATE = "date";
+    private static final String COMMENTAIRE = "commentaire";
+    private static final String NOTIFICATION = "notification";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "CREATE TABLE if not exists "
+            + TABLE_EVENEMENTS  + "("
+                + COLUMN_ID     + " INTEGER primary key auto_increment, "
+                + LIBELLE       + " TEXT not null, "
+                + DATE          + " TEXT not null, "
+                + COMMENTAIRE   + " TEXT, "
+                + NOTIFICATION  + " INTEGER not null)" ;
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        try{
+            sqLiteDatabase.execSQL(DATABASE_CREATE);
+        }
+        catch(SQLException e )
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        Log.w(DataBaseHelper.class.getName(), "upgrading DATABASE from version "+ i + " to " + i1 + ", wich will destroy all data.");
+
+        sqLiteDatabase.execSQL("DROP TABLE if exists " + TABLE_EVENEMENTS);
+
+        onCreate(sqLiteDatabase);
+    }
+}
